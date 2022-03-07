d3f6023c62be993723cee29ebeb6cff7f61a4377
Kimia Montazeri <kimia.montazeri@yahoo.com>
2022-03-07 17:46:35 +0330

    Add better version of mealy seq detector

--- 03-SeqCircuits/seq-detector-1101-mealy/mealy_seq_detector.vhd
+++ 03-SeqCircuits/seq-detector-1101-mealy/mealy_seq_detector.vhd
@@ -9,6 +9,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity mealy_seq_detector is
 	Port (
 		input: in STD_LOGIC;
+		reset: in STD_LOGIC;
 		clk: in STD_LOGIC;
 		output: out STD_LOGIC
 	);
@@ -17,62 +18,69 @@ end mealy_seq_detector;
 architecture Behavioral of mealy_seq_detector is
 	type state is (s0, s1, s2, s3, s4);
 	signal present_state : state := s0;
+	signal next_state : state := s0;
 begin
-
-	detecting_process : process(clk, input)
+	clk_process : process (clk)
 	begin
-		-- default value of output (to avoid undifined value at the first 10ns of simulation)
-		output <= '0'; 
-		
 		if rising_edge(clk) then
-			case (present_state) is
-				when s0 =>
-					if (input = '1') then
-						present_state <= s1;
-						output <= '0';
-					else 
-						present_state <= s0;
-						output <= '0';
-					end if;
-			
-				when s1 => 
-					if (input = '1') then
-						present_state <= s2;
-						output <= '0';
-					else
-						present_state <= s0;
-						output <= '0';
-					end if;
-			
-				when s2 => 
-					if (input = '0') then
-						present_state <= s3;
-						output <= '0';
-					else 
-						present_state <= s2;
-						output <= '0';
-					end if;
-				
-				when s3 =>
-					if (input = '1') then
-						present_state <= s4;
-						output <= '1';
-					else
-						present_state <= s0;
-						output <= '0';
-					end if;
-			
-				when s4 =>
-					if (input = '1') then
-						present_state <= s2;
-						output <= '0';
-					else 
-						present_state <= s0;
-						output <= '0';
-					end if;
-		
-			end case;
+			if (reset = '0') then
+				present_state <= s0;
+			else
+				present_state <= next_state;
+			end if;
 		end if;
 	end process;
+	
+	detecting_process : process(present_state, input)
+	begin
+		output <= '0'; 
+		case (present_state) is 
+			when s0 =>
+				if (input = '1') then
+					next_state <= s1;
+					output <= '0';
+				else
+					next_state <= s0;
+					output <= '0'; 
+				end if; 
+			when s1 =>
+				if (input = '1') then
+					next_state <= s2;
+					output <= '0';
+				else
+					next_state <= s0;
+					output <= '0'; 
+				end if; 
+			when s2 =>
+				if (input = '1') then
+					next_state <= s2;
+					output <= '0';
+				else
+					next_state <= s3;
+					output <= '0'; 
+				end if; 
+			when s3 =>
+				if (input = '1') then
+					next_state <= s4;
+					output <= '1';
+				else
+					next_state <= s0;
+					output <= '0'; 
+				end if; 
+			when s4 =>
+				if (input = '1') then
+					next_state <= s2;
+					output <= '1';
+				else
+					next_state <= s0;
+					output <= '0'; 
+				end if; 
+			when others =>
+				next_state <= s0;
+				output <= '0';
+		end case;
+	end process;
+
+
 end Behavioral;
 
--- 03-SeqCircuits/seq-detector-1101-mealy/mealy_seq_detector_tb.vhd
+++ 03-SeqCircuits/seq-detector-1101-mealy/mealy_seq_detector_tb.vhd
@@ -1,9 +1,10 @@
-----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- Company: AUT
 -- Engineer: Kimia Montazeri & Arian Ghazvini
-----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
+
  
 ENTITY mealy_seq_detector_tb IS
 END mealy_seq_detector_tb;
@@ -15,6 +16,7 @@ ARCHITECTURE behavior OF mealy_seq_detector_tb IS
     COMPONENT mealy_seq_detector
     PORT(
          input : IN  std_logic;
+         reset : IN  std_logic;
          clk : IN  std_logic;
          output : OUT  std_logic
         );
@@ -23,25 +25,27 @@ ARCHITECTURE behavior OF mealy_seq_detector_tb IS
 
    --Inputs
    signal input : std_logic := '0';
+   signal reset : std_logic := '0';
    signal clk : std_logic := '0';
 
  	--Outputs
    signal output : std_logic;
 
    -- Clock period definitions
-   constant clk_period : time := 20 ns;
+   constant clk_period : time := 10 ns;
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: mealy_seq_detector PORT MAP (
           input => input,
+          reset => reset,
           clk => clk,
           output => output
         );
 
    -- Clock process definitions
-   clk_process : process
+   clk_process :process
    begin
 		clk <= '0';
 		wait for clk_period/2;
@@ -50,14 +54,15 @@ BEGIN
    end process;
  
 
- 
    -- Stimulus process
    stim_proc: process
    begin		
 
 		wait for 100 ns;
+		reset <= '0';
 		
 		--011011010011011
+		reset <= '1';
 		input <= '0';
 		wait for clk_period;
 		
